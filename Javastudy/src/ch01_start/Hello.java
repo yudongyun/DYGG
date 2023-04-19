26fc094082faee62b1538fa8dc44b9c0fc2a4f3e
pc13 <pc13@pc13>
2023-04-19 14:06:01 +0900

    2023-04-19 test

--- Javastudy/src/ch01_start/Hello.java
+++ Javastudy/src/ch01_start/Hello.java
@@ -3,6 +3,7 @@ package ch01_start;
 public class Hello {
 
 	public static void main(String[] args) {
+		// 억울함
        System.out.println("Hello World");
        // syso + ctrl+space
        // 실행 단축키 [ctrl+ F11]
